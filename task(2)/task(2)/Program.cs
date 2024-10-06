b05c0255e70a31c50a630779037cd303ba7825f3
Haneen-Sobeeh <132911676+Haneen-Sobeeh@users.noreply.github.com>
2024-10-06 20:01:02 +0300

    Add files via upload

---
+++ task(2)/task(2)/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("enter the number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int smaller;
+
+            if (num1 > num2)
+            {
+                smaller = num2;
+            }
+            else
+            {
+                { smaller = num1; }
+            }
+            Console.WriteLine($"the smaller number is {smaller}");
+            Console.WriteLine("-----------------------------------------------------------");
+
+
+            //2
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine($"the sign is +");
+            }
+            else
+            {
+                Console.WriteLine("the sign is -");
+
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+
+
+            //3
+            int[] numbers = new int[3];
+            numbers[0] = Convert.ToInt32(Console.ReadLine());
+            numbers[1] = Convert.ToInt32(Console.ReadLine());
+            numbers[2] = Convert.ToInt32(Console.ReadLine());
+
+            Array.Sort(numbers);
+            Console.WriteLine("Sorted numbers: {0}, {1}, {2}", numbers[0], numbers[1], numbers[2]);
+            Console.WriteLine("-----------------------------------------------------------");
+
+            //4
+            int[] num = { 2, 5, -2, 6, 2, -11 };
+            int max = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > max) { }
+                max = num[i];
+            }
+            Console.WriteLine(max);
+            Console.WriteLine("-----------------------------------------------------------");
+
+            //5
+            Console.Write("Input kilometers per hour: ");
+            double K = double.Parse(Console.ReadLine());
+
+            double mph = K * 0.621371;
+
+            Console.WriteLine($"{K} kilometers per hour is {mph} miles per hour");
+             Console.WriteLine("-----------------------------------------------------------");
+
+
+           //6
+            Console.Write("Input hours: ");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.Write("Input minutes: ");
+            int m = int.Parse(Console.ReadLine());
+
+            int total = (h * 60) + m;
+
+            Console.WriteLine("Total: {0} minutes.", total);
+           Console.WriteLine("-----------------------------------------------------------");
+
+
+            //7
+            Console.Write("Input minutes: ");
+            int Total = int.Parse(Console.ReadLine());
+
+            int hs = total / 60;
+            int min = total % 60;
+
+             Console.WriteLine("-----------------------------------------------------------");
+
+            //8
+            string[] s = { "Hello world", "Hi,im haneen", "Programming", "Jordan", "have a nice time " };
+
+            foreach (string sentence in s)
+            {
+                Console.WriteLine(sentence.PadRight(20, ' '));
+             
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+
+
+
+            //9
+
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+
+            string result = ReverseOddLengthWords(input);
+            Console.WriteLine("Modified sentence: " + result);
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+
+        // Method to reverse odd-length words
+        static string ReverseOddLengthWords(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length % 2 != 0)
+                {
+                    char[] charArray = words[i].ToCharArray();
+                    Array.Reverse(charArray);
+                    words[i] = new string(charArray);
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+
+    }
+}
+
---
+++ task(3)/task(3)/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            string[] ARR = { "1", "7", "9", "45" };
+            string[] arr2 = { "Str", "alex", "moh" };
+            string[] arr3 = { "the", "fox", "over", "lazy", "dog" };
+            Console.WriteLine("-----------------------------------------------------");
+
+
+            //2
+            string[] fruits = { "Tomato", "Banana", "Watermelon" };
+            Console.WriteLine("Index of \"Banana\": 1\r\nIndex of \"Tomato\": 0");
+            Console.WriteLine("-----------------------------------------------------");
+
+
+            //3
+            string[] Food = { "Pizza", "Sweet", "Burger", "Mansaf", "Ice Cream" };
+            string[] Sport = { "Football", "Basketball", "Tennis" };
+            string[] Movies = { "Movie", "Movie", "Movie", "Movie" };
+
+            Console.WriteLine("Favorite Food:");
+            foreach (string f in Food)
+            {
+                Console.WriteLine(f);
+            }
+
+            Console.WriteLine("\nFavorite Sport:");
+            foreach (string s in Sport)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("\nFavorite Movies:");
+            foreach (string m in Movies)
+            {
+                Console.WriteLine(m);
+            }
+            Console.WriteLine("\nFavorite Sport Again:");
+            for (int i = 0; i < Sport.Length; i++)
+            {
+                Console.WriteLine(Sport[i]);
+            }
+            Console.WriteLine("-----------------------------------------------------");
+
+
+            //4
+            Console.WriteLine("enter the numbers");
+            string num = Console.ReadLine();
+
+
+            string[] numbers = num.Split(',');
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+            int num3 = int.Parse(numbers[2]);
+
+            int sum = num1 + num2 + num3;
+            Console.WriteLine("The sum of three numbers: " + sum);
+            Console.WriteLine("-----------------------------------------------------");
+
+            //5
+            int sm = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                    sm += i;
+            }
+            Console.WriteLine($"the sum of odd number is {sm}");
+            Console.WriteLine("-----------------------------------------------------");
+
+
+            //6
+
+            int n = 3;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write("");
+                }
+
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+
+
+            //7
+            int z = 4;
+                int number = 1;
+
+                for (int a = 1; a <= z; a++)
+                {
+                    // Print leading spaces
+                    for (int j = a; j < z; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // Print numbers
+                    for (int k = 1; k <= a; k++)
+                    {
+                        Console.Write(number + " ");
+                        number++;
+                    }
+                    Console.WriteLine();
+                }
+
+
+
+            }
+        }
+    }
+
+
